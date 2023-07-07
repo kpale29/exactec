9049d4bd95e4f68aaf6508200c75ada81a0485af
Kevin Ricardo Palencia Gil <kevingil1910@gmail.com>
2023-07-07 06:39:46 +0000

    noup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://kpale29.github.io',
-    base: '/exactec.github.io',
+    base: '/exactec',
 });
 
 

f4e28c35724cab113e94e8587c2b10be5127b15d
kpale29 <kevingil1910@gmail.com>
2023-07-12 20:52:28 -0600

    ,

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kpale29.github.io',
-    base: '/exactec.github.io',
+  site: 'https://kpale29.github.io',
+  base: '/exactec.github.io',
+  integrations: [tailwind()]
 });
-
-

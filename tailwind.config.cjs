69addf1a37b281c750f24a13293101e8822eeb79
kpale29 <kevingil1910@gmail.com>
2023-07-12 20:57:00 -0600

    working :whale:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				'primary-bg' : '#f3f3f3',
+				'secondary-bg': '#fff',
+				'primary-text' : '#2f2f2f',
+			}
+
+		},
 	},
 	plugins: [],
 }

80c7a31865177a81c4542d50773f54f16505b072
JoseCruz <jose.crperez@gmail.com>
2023-08-29 10:39:20 -0600

    feat(carousel product): add new feature

--- src/components/Productos/CardProduct.tsx
+++ src/components/Productos/CardProduct.tsx
@@ -1,14 +1,47 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './css/CardProduct.css';
+// import required modules
+import { Autoplay, Zoom, Navigation } from 'swiper/modules';
+
 export default function CardProduct({ img, name, description, items }) {
   return (
     <div className="pb-16 lg:flex">
-      <div className="flex w-full items-center justify-center border border-black lg:w-auto lg:flex-none">
-        <figure>
-          <img
-            className="w-[210px] max-w-[310px] lg:w-[314px] lg:max-w-[314px] "
-            src={img}
-            alt="Tecnología de vanguardia"
-          />
-        </figure>
+      <div className="flex items-center justify-center border border-black w-[310px] lg:flex-none">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          zoom={true}
+          navigation={true}
+          modules={[Zoom,Autoplay, Navigation]}
+          className="mySwiper"
+        >
+          {img.map((image)=>(
+            <SwiperSlide>
+               <div className="swiper-zoom-container">
+              <figure>
+                <img
+                  className="w-[210px] max-w-[310px] lg:w-[314px] lg:max-w-[314px] "
+                  src={image}
+                  alt="Tecnología de vanguardia"
+                />
+              </figure>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        
       </div>
       <div className="pt-5 lg:pl-5 lg:pt-0">
         <p className="bold text-xl">{name}</p>
